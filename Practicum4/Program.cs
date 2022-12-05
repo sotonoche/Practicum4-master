f1cdb242a06081e2e61a9be3cc5a8216e4f68e7a
stu-pksp120 <stu-pksp120@AMDVLA3100W10>
2022-12-05 13:20:51 +0300

    fix

--- Practicum4/Program.cs
+++ Practicum4/Program.cs
@@ -10,13 +10,21 @@ namespace Practicum4
     {
         static double f(double n, double m)
         {
-            if (n == 0)
-                return m + 1;
-            else
+            try
+            {
+                if (n == 0)
+                    return m + 1;
+                else
             if ((n != 0) && (m == 0))
-                return f(n - 1, 1);
-            else
-                return f(n - 1, f(n, m - 1));
+                    return f(n - 1, 1);
+                else
+                    return f(n - 1, f(n, m - 1));
+            }
+            catch (StackOverflowException)
+            {
+                Console.WriteLine("Стек переполнен. Значение на выходе слишком большое!");
+            }
+            
         }
         
         static void Main(string[] args)
